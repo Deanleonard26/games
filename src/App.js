bdf61f38b0edbb02c16bef5b3697942cf39c03bc
Dean Leonard <deanleonard26@gmail.com>
2021-04-19 17:19:54 +0100

    Dean start

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { useDispatch } from "react-redux";
+import { loadGames } from "./actions/gamesAction";
+// Components & Pages
+import Home from './pages/Home'
+
+function App() {
+  
+  return (
+    <div className="App">
+      <h1>Hello Ignite</h1>
+      <Home />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/api.js
@@ -0,0 +1,40 @@
+//Base URL
+const base_url = "https://api.rawg.io/api/";
+
+// const apiKey = 'c7dd18ceb01d4ebca972d9b8b41d6dd9';
+
+//Getting the date
+const getCurrentMonth = () => {
+  const month = new Date().getMonth() + 1;
+  if (month < 10) {
+    return `0${month}`;
+  } else {
+    return month;
+  }
+};
+//Getting the date
+const getCurrentDay = () => {
+  const day = new Date().getDate();
+  if (day < 10) {
+    return `0${day}`;
+  } else {
+    return day;
+  }
+};
+
+//Current day/month/year
+const currentYear = new Date().getFullYear();
+const currentMonth = getCurrentMonth();
+const currentDay = getCurrentDay();
+const currentDate = `${currentYear}-${currentMonth}-${currentDay}`;
+const lastYear = `${currentYear - 1}-${currentMonth}-${currentDay}`;
+const nextYear = `${currentYear + 1}-${currentMonth}-${currentDay}`;
+
+//Popular Games
+const popular_games = `games.json?dates=${lastYear},${currentDate}&ordering=-rating&page_size=10`;
+const upcoming_games = `games.json?dates=${currentYear},${nextYear}&ordering=-added&page_size=10`;
+const new_games = `games.json?dates=${lastYear},${currentYear}&ordering=-released&page_size=10`
+
+export const popularGamesURL = () => `${base_url}${popular_games}`;
+export const upcomingGamesURL = () => `${base_url}${upcoming_games}`;
+export const newGamesURL = () => `${base_url}${new_games}`
